48f3c8b2b536fdfad69e321ea3d2ae3fee96a83e
2014218413@qq.com <2014218413@qq.com>
2020-08-17 22:49:50 +0800

    20200817

---
+++ src/main/java/dream/SpringBootStartApplication.java
@@ -0,0 +1,18 @@
+package dream;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @author 忌冷忌热的怕热不怕冷的吃不胖少年
+ * 回首向来萧瑟处，也无风雨也无晴。
+ **/
+/*修改启动类，继承SpringBootServletInitializer类并重写configure方法 */
+public class SpringBootStartApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        //注意这里的参数要指向原先用main方法执行的Application启动类
+        return builder.sources(Application_.class);
+    }
+}
