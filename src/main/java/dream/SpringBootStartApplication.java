2daba62543860b5032bbb01dfd23ce3329225139
2014218413@qq.com <2014218413@qq.com>
2020-08-18 11:10:08 +0800

    ss

--- src/main/java/dream/SpringBootStartApplication.java
+++ src/main/java/dream/SpringBootStartApplication.java
@@ -8,11 +8,14 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  * @author 忌冷忌热的怕热不怕冷的吃不胖少年
  * 回首向来萧瑟处，也无风雨也无晴。
  **/
-/*修改启动类，继承SpringBootServletInitializer类并重写configure方法 */
+/*修改启动类，继承SpringBootServletInitializer类并重写configure方法
+*
+* 这是将 springboot项目部署到外置tomcat上需要设置的
+* */
 public class SpringBootStartApplication extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        //注意这里的参数要指向原先用main方法执行的Application启动类
-        return builder.sources(Application_.class);
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        //注意这里的参数要指向原先用main方法执行的Application启动类
+//        return builder.sources(Application_.class);
+//    }
 }
